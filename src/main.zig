671bc7e47f36ff14f89b61ced0315e8e8b47f7e3
mainak55512 <mbhattacharjee432@gmail.com>
2025-03-08 21:58:23 +0530

    Added CLI features

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,49 @@ pub fn main() !void {
     };
     const newAlloc = gpa.allocator();
 
-    try huff.huffEncoder(newAlloc, "test.txt", "test.mhuff");
-    try huff.huffDecoder(newAlloc, "test.mhuff", "decoded.txt");
+    const args = try std.process.argsAlloc(newAlloc);
+    defer std.process.argsFree(newAlloc, args);
+
+    if (args.len == 3) {
+        var fileNameParts = std.ArrayList([]const u8).init(newAlloc);
+        defer fileNameParts.deinit();
+        var it = std.mem.splitScalar(u8, args[2], '.');
+        while (it.next()) |elem| {
+            try fileNameParts.append(elem);
+        }
+        const ext = fileNameParts.pop();
+        if (std.mem.eql(u8, args[1], "encode")) {
+            if (!std.mem.eql(u8, ext, "mhuff")) {
+                try fileNameParts.append(".mhuff");
+                var outFile = std.ArrayList(u8).init(newAlloc);
+                defer outFile.deinit();
+                for (fileNameParts.items) |elem| {
+                    try outFile.appendSlice(elem);
+                }
+                const outFileName = try outFile.toOwnedSlice();
+                defer newAlloc.free(outFileName);
+                try huff.huffEncoder(newAlloc, args[2], outFileName);
+            } else {
+                std.log.err("File can't be encoded.\n", .{});
+            }
+        } else if (std.mem.eql(u8, args[1], "decode")) {
+            if (std.mem.eql(u8, ext, "mhuff")) {
+                try fileNameParts.append("_decoded.txt");
+                var outFile = std.ArrayList(u8).init(newAlloc);
+                defer outFile.deinit();
+                for (fileNameParts.items) |elem| {
+                    try outFile.appendSlice(elem);
+                }
+                const outFileName = try outFile.toOwnedSlice();
+                defer newAlloc.free(outFileName);
+                try huff.huffDecoder(newAlloc, args[2], outFileName);
+            } else {
+                std.log.err("File can't be decoded.\n", .{});
+            }
+        } else {
+            std.log.err("Illegal command.\n", .{});
+        }
+    } else {
+        std.log.err("Incorrect number of arguments.\n", .{});
+    }
 }
